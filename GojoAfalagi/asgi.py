4018e7fa3b55cd73638706cfbe55104d3a4d2979
ezekielalene <ezkielmisgae@gmail.com>
2022-12-26 01:17:58 +0300

    Edit upload

--- GojoAfalagi/asgi.py
+++ GojoAfalagi/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Gojo-Afalagi project.
+ASGI config for GojoAfalagi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gojo-Afalagi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GojoAfalagi.settings')
 
 application = get_asgi_application()
--- GojoAfalagi/wsgi.py
+++ GojoAfalagi/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gojo-Afalagi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GojoAfalagi.settings')
 
 application = get_wsgi_application()
 app = application
