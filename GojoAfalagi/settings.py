f843d406bb7f20f1b0db8762f815d750a8179a36
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2022-12-26 16:07:33 +0300

    Update settings.py

--- GojoAfalagi/settings.py
+++ GojoAfalagi/settings.py
@@ -75,13 +75,6 @@ WSGI_APPLICATION = 'GojoAfalagi.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Eazy@200122922',
-        'HOST': 'localhost',
-        'PORT': '5432',
         # 'default': {
         #     'ENGINE': 'django.db.backends.sqlite3',
         #     'NAME': BASE_DIR / 'db.sqlite3',
