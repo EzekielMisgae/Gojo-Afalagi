e4293d8925bee8eeded318d92cc08e7314b63ab4
ezekielalene <ezkielmisgae@gmail.com>
2023-01-08 00:50:28 +0300

    files

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,15 @@
-# build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+#!/bin/bash
+
+#Bulid the project
+echo "Initiating virtualEnv..."
+python3.9 source virtualEnvironment/bin/activate
+
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make migrations..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect static..."
+python3.9 manage.py collectstatic --noinput --clear
