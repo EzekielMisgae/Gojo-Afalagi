46499947646189f85bc6720134e40e59a066ad23
ezekielalene <ezkielmisgae@gmail.com>
2023-01-07 04:59:02 +0300

    gitignore

--- GojoAfalagi/settings.py
+++ GojoAfalagi/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*+%lw7j=bnmaj)b31h31)n!1i@jki-^lvmoa)22&qg3arh^c77'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
