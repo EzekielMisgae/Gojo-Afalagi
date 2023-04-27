145dbf7fdc3ad0f72260c33a0ca0c7ad81a2142a
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2023-04-27 22:20:33 +0300

    URL update for main URL extension

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,9 @@
+#URLs for the project
+
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-
 
 urlpatterns = [
     path('', views.landingPage, name='landingPage'),
