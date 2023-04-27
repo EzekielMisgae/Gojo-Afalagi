0e4af73f1f20ba98ce1cd6384c5f3ed9ba5fbfa6
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2023-04-27 22:07:45 +0300

    Comments edit on script

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: OnePage - v4.9.2
-* Template URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
